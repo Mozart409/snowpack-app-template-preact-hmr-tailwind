584622d8b6157ac1e2eac783726572b96582ed44
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-10-21 23:46:04 +0200

    init

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,7 +3,17 @@ module.exports = {
     public: '/',
     src: '/_dist_',
   },
-  plugins: ['@snowpack/plugin-dotenv', '@prefresh/snowpack'],
+  plugins: [
+    '@snowpack/plugin-dotenv',
+    '@prefresh/snowpack',
+    [
+      '@snowpack/plugin-optimize',
+      {
+        preloadModules: true,
+      },
+    ],
+  ],
+  scripts: { 'build:css': 'postcss' },
   install: [
     /* ... */
   ],
--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,53 @@
 import { h } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
-import logo from './logo.png';
-import './App.css';
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://preactjs.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Preact
-          </a>
-        </p>
-      </header>
+    <div className="flex flex-col min-h-screen font-sans">
+      <div className="flex-1 w-full p-4 mx-auto max-w-7xl md:px-8 md:py-16">
+        <h1 className="text-center bg-gradient-to-r from-blue-400  to-teal-400 text-2xl font-bold leading-7 bg-clip-text text-transparent sm:text-3xl sm:leading-9 sm:truncate">
+          Snowpack + Preact + HMR + Tailwind
+        </h1>
+        <div className="h-full flex flex-wrap md:flex-no-wrap justify-center items-center my-12 py-12 px-4 sm:px-8">
+          <div className="flex flex-wrap">
+            <div>
+              <img
+                className="w-40"
+                src="./snowpack-logo-black.png"
+                alt="snowpack-logo"
+              />
+              <img
+                className="w-40 mt-2"
+                src="./snowpack-wordmark-black.png"
+                alt="snowpack-wordmark"
+              />
+            </div>
+            <div className="w-24 my-auto text-gray-700">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                />
+              </svg>
+            </div>
+            <img className="w-40" src="./preact.png" alt="preact" />
+          </div>
+          <p>
+            Edit{' '}
+            <code className="bg-gradient-to-r from-orange-400 via-red-500 to-pink-500 bg-clip-text text-transparent">
+              src/App.jsx
+            </code>{' '}
+            and save to reload.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
