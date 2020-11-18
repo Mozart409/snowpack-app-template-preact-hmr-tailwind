74d95864b6689f37c9a811e02d06fa18fc999ca6
Amadeus <maderamadeus@gmail.com>
2020-11-18 16:31:50 +0100

    added tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,14 @@ import { h } from 'preact';
 import { render } from '@testing-library/preact';
 import App from './App';
 
-test('renders learn preact link', () => {
+test('renders Snowpack text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn preact/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/Snowpack/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders preact text', () => {
+  const { getByText } = render(<App />);
+  const textElement = getByText(/Preact/i);
+  expect(textElement).toBeInTheDocument();
 });
